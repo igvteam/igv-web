2c3404f6aa3b18ec35205bbd9f543e12b6cd86c8
turner <douglass.turner@gmail.com>
2024-08-15 11:01:33 -0400

    Refactor url modal creation

--- js/widgets/loadWidget.js
+++ js/widgets/loadWidget.js
@@ -3,39 +3,11 @@ import FileLoadManager from "./fileLoadManager.js"
 import FileLoadWidget from "./fileLoadWidget.js"
 import * as Utils from "./utils.js"
 import AlertSingleton from "./alertSingleton.js"
+import {createURLModalElement} from "./urlModal.js"
 
 function createURLWidget(igvMain, urlModalId, urlModalTitle, loadHandler) {
 
-    const html =
-        `<div id="${ urlModalId }" class="modal fade" tabindex="-1">
-
-        <div class="modal-dialog modal-lg">
-    
-            <div class="modal-content">
-    
-                <div class="modal-header">
-                    <div class="modal-title">${ urlModalTitle }</div>
-    
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    
-                </div>
-    
-                <div class="modal-body">
-                </div>
-    
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">OK</button>
-                </div>
-    
-            </div>
-    
-        </div>
-
-    </div>`
-
-    const fragment = document.createRange().createContextualFragment(html)
-    const urlModalElement = fragment.firstChild
+    const urlModalElement = createURLModalElement(urlModalId, urlModalTitle)
 
     igvMain.appendChild(urlModalElement)
 
